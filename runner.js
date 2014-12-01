b7cf518fc8dc7fc99fd4fd80ce007ef8e5b3cb0e
Davis Yarbrough <dyarbrough93@yahoo.com>
2014-11-30 19:39:36 -0600

    Update runner.js

--- runner.js
+++ runner.js
@@ -1,60 +1,84 @@
 /*
  * Script for a runner creep
- *
  */
  
-module.exports = function(creep) {
+module.exports = {
     
-    if (creep.energy < creep.energyCapacity) {
+    run: function(creep) {
+    
+        var nearest_enemy = creep.pos.findNearest(Game.HOSTILE_CREEPS);
         
-        var tgt_pos = creep.memory.target_pos;
-        if (tgt_pos) {
-            if (creep.pos.isNearTo(tgt_pos)) {
-                var tgt_energy = creep.pos.findNearest(Game.DROPPED_ENERGY);
-                creep.pickup(tgt_energy);
-                creep.memory.tgt_pos = null;
-                return;
-            } 
-            else {
-                creep.moveTo(tgt_pos);
-                return;
-            }
+        if (nearest_enemy && creep.pos.isNearTo(nearest_enemy)) {
+            var enemy_dir = creep.pos.getDirectionTo(nearest_enemy);
+            console.log(creep.move(require('getOppositeDirection')(enemy_dir)));
         }
         
-        var dropped_energy = creep.room.find(Game.DROPPED_ENERGY);
-        var largest_dropped_energy = 0, energy_target = null;
-        for (var i in dropped_energy) {
-            if (dropped_energy[i].energy > largest_dropped_energy) {
-                largest_dropped_energy = dropped_energy[i].energy;
-                energy_target = dropped_energy[i];
-            }
-        }
+        var big_daddy_name;
+        if (!creep.memory.big_daddy) 
+            big_daddy_name = this.assign_daddy(creep);
+        else
+            big_daddy_name = creep.memory.big_daddy;
         
-        if (energy_target) {
-            creep.memory.target_pos = {
-                x: energy_target.pos.x,
-                y: energy_target.pos.y
-            };
-            creep.moveTo(energy_target);
-            creep.pickup(energy_target);
-        }
-        else {
-            var nearest_miner = creep.pos.findNearest(Game.MY_CREEPS, {
-                filter: function(_creep) {
-                    return _creep.memory.role === 'miner';
-                }
-            });
+        var big_daddy = Game.creeps[big_daddy_name];
+        
+        if (!big_daddy) { // big daddy dead
             
-            if (nearest_miner) 
-                creep.moveTo(nearest_miner);
-            else
-                creep.moveTo(creep.pos.findNearest(Game.MY_SPAWNS));
+            for (var i in Game.creeps) {
+                var _creep = Game.creeps[i];
+                if (_creep.memory.role === 'miner' && !_creep.memory.little_sister) {
+                    _creep.memory.little_sister = creep.name;
+                    creep.memory.big_daddy = _creep.name;
+                    return;
+                }
+            }
+            
+            creep.memory.big_daddy = null;
+            creep.moveTo(creep.pos.findNearest(Game.MY_SPAWNS));
+            
+            // TODO: add new big daddy to spawn queue
+            // ALSO TODO: make spawn queue.
+            return;
         }
-    }
-    else {
-        var nearest_spawn = creep.pos.findNearest(Game.MY_SPAWNS);
         
-        creep.moveTo(nearest_spawn);
-        creep.transferEnergy(nearest_spawn);
+        if (creep.energy < creep.energyCapacity) {
+            
+            creep.moveTo(big_daddy.pos);
+            
+            if (creep.pos.isNearTo(big_daddy.pos)) {
+                
+                var _energy = creep.pos.findNearest(Game.DROPPED_ENERGY);
+                creep.pickup(_energy);
+                
+            }
+        }
+        
+        else {
+            var nearest_spawn = creep.pos.findNearest(Game.MY_SPAWNS);
+            creep.moveTo(nearest_spawn);
+            creep.transferEnergy(nearest_spawn);
+        }
+    },
+    
+    /*
+     * Assign runner creep to a miner creep
+     * @param creep The creep to assign
+     * @return The miner creep name, null upon failure
+     */
+    assign_daddy: function(creep) {
+        
+        for (var i in Game.creeps) {
+            
+            var _creep = Game.creeps[i];
+            
+            if (_creep.memory.role === 'miner' && 
+                !_creep.memory.little_sister) {
+            
+                creep.memory.big_daddy = _creep.name;
+                _creep.memory.little_sister = creep.name;
+                return _creep.name;
+            }
+        }
+        
+        return null;
     }
 };
